7e6b47d58db79f719ac401de67557b9421ae4b1a
312-12\jskim <jscode77@gmail.com>
2024-12-24 17:01:08 +0900

    클래스, 모듈

---
+++ python-exam/mymod/mod2.py
@@ -0,0 +1,8 @@
+PI = 3.1415926535           # 필드
+
+class Math:                 # 클래스
+    def solve(self, r):     # 메서드
+        return PI * (r**2)
+
+def add(a, b) :             # 함수
+    return a + b
